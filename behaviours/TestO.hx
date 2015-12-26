20017909447498561f596cceaba9caca28f5ef77
TBaudon <baudon.thomas@gmail.com>
2015-12-26 04:43:38 +0100

    first behaviour

---
+++ behaviours/TestO.hx
@@ -0,0 +1,13 @@
+package behaviours;
+
+import fr.radstar.engine.Behaviour;
+
+class TestO extends Behaviour {
+
+  public var color : UInt;
+
+  override public function init(){
+    trace("lol");
+  }
+
+}
