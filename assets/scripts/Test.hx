6eb03244dbddc0e41c524099a005e89b130cae56
Thomas BAUDON <baudon.thomas@gmail.com>
2015-12-28 12:32:29 +0100

    behaviour and scripting is way too laggy.

--- assets/scripts/Test.hx
+++ assets/scripts/Test.hx
@@ -1,18 +1,25 @@
 import("Math");
+import("openfl.Lib");
 import("fr.radstar.Game");
 
 var color = 210;
 var transform;
-var t = 0;
+var t = Math.random() * 1000;
+var sprite = null;
+
+var startX = 0;
+var stratY = 0;
 
 function init(){
-  trace(color);
-  transform = this.getComponent("Transform");
+	transform = this.getComponent("Transform");
+	startX = Math.random() * Lib.current.stage.stageWidth;
+	startY = Math.random() * Lib.current.stage.stageHeight;
+	transform.x = startX;
+	transform.y = startY;
 }
 
 function update(){
-  t+=Game.smoothedDelta;
-  transform.x = Math.sin(t) * 200 + 338;
-  transform.y = Math.sin(t*5) * 30+50;
-
+	t+=Game.smoothedDelta;
+	transform.x = startX + Math.sin(t) * 200;
+	transform.y = startY + Math.sin(t*5) * 30;
 }
